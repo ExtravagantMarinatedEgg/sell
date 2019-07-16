553e633b5cb5c1926c510dc5c7a0687bc47249a6
Tong <m13302061411@163.com>
2019-07-16 11:38:43 +0800

    微信手工

---
+++ src/main/java/com/marinatedegg/sell/controller/WeixinController.java
@@ -0,0 +1,31 @@
+package com.marinatedegg.sell.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.xml.ws.Response;
+
+@RestController
+@RequestMapping("/weixin")
+@Slf4j
+public class WeixinController {
+
+
+    @GetMapping("/auth")
+    public void auth(@RequestParam("code") String code, @RequestParam("state") String state) {
+        log.info("进入auth方法 code={}, state={}", code, state);
+
+        String url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=wxc88f38cab217c2bb&secret=1eeccff772b2df152ddae9fb416087e2&code=" + code + "&grant_type=authorization_code";
+
+        RestTemplate restTemplate = new RestTemplate();
+        String response = restTemplate.getForObject(url, String.class);
+        log.info("response={}", response);
+
+    }
+
+
+}
